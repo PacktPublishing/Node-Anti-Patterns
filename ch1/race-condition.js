5b67bb76018da90d66d694cfaaa0b5ee46e83d1a
Lucas Santos <hello@lsantos.dev>
2024-10-15 17:59:46 +0200

    feat(ch1): initial files

---
+++ ch1/race-condition.js
@@ -0,0 +1,14 @@
+import { writeFile, stat, unlink } from 'node:fs/promises'
+import { dirname, join, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+async function raceCondition() {
+    const filename = join(resolve(dirname(fileURLToPath(import.meta.url)), './race.txt'))
+    await unlink(filename)
+
+    writeFile(filename, 'Written from first promise\n', { flag: 'a' })
+    writeFile(filename, 'Written from second promise\n', { flag: 'a' })
+}
+
+raceCondition().then(() => console.log('Race condition completed'))
+
